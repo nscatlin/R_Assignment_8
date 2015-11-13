2ab9e64cc61a6c2c07290fd82f6d25f637e7c98e
Nathan Catlin <ncatlin@ufl.edu>
2015-11-13 13:08:38 -0500

    Put in some pseudo code.

--- R_Assignment_8.R
+++ R_Assignment_8.R
@@ -1,10 +1,21 @@
+library(dpylr)
+library(stringr)
+
 mammal_sizes <- read.csv("MOMv3.3.txt", sep = "\t", header = FALSE, stringsAsFactors = FALSE, na.strings = "-999")
 
 colnames(mammal_sizes) <- c("continent", "status", "order", 
                             "family", "genus", "species", "log_mass", "combined_mass", 
                             "reference")
 
-head(mammal_sizes)
+#the above wash pushed to the Github Repostitory named 'R_Assignment_8'
+
+#1.2, 
+# - calculate mean mass of extinct species
+# - calculate mean mass extant species
+
+mean_mass_extinct <- if(mammal_sizes$status == "extinct")
+
+
 
 
 
