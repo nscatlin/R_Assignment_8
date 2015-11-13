6fda3bb93bad708dc6632dcfdfe2b34b333978f2
Nathan Catlin <ncatlin@ufl.edu>
2015-11-13 12:24:38 -0500

    This is code in order to start the assignment.

---
+++ R_Assignment_8.R
@@ -0,0 +1,9 @@
+mammal_sizes <- read.csv("MOMv3.3.txt", sep = "\t", header = FALSE, stringsAsFactors = FALSE, na.strings = "-999")
+
+colnames(mammal_sizes) <- c("continent", "status", "order", 
+                            "family", "genus", "species", "log_mass", "combined_mass", 
+                            "reference")
+
+head(mammal_sizes)
+
+
